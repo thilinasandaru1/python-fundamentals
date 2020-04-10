97a11131f27b5b32150dc39eba75ba3359ee553b
thilinasandaru1 <thilinasandaru1@gmail.com>
2020-04-10 08:34:53 +0530

    Conditionals :bookmark:

---
+++ 009-conditionals.py
@@ -0,0 +1,72 @@
+# If / Else conditions are used to decide to do something based on
+# something being true or false
+
+x = 10
+y = 5
+
+# Comparison operators (== , !=, >, <, >=, <=)
+
+# if
+if x == y:
+    print(f'{x} is equal to {y}')
+
+# if / else
+if x > y:
+    print(f'{x} is greater than {y}')
+else:
+    print(f'{x} is less than or equal to {y}')
+
+# elif
+if x > y:
+    print(f'{x} is greater than {y}')
+elif x == y:
+    print(f'{x} is equal to {y}')
+else:
+    print(f'{x} is less than {y}')
+
+# nested if
+if x > 2:
+    if x <= 10:
+        print(f'{x} is greater than 2 and less than or equal to 10')
+
+
+''' LOGICAL OPERATORS '''
+
+# and
+if x > 2 and x <= 10:
+    print(f'{x} is greater than 2 and less than or equal to 10')
+
+# or
+if x > 2 or x <= 10:
+    print(f'{x} is greater than 2 or less than or equal to 10')
+
+# not
+if not(x == y):
+    print(f'{x} is not equal to {y}')
+
+
+''' MEMBERSHIP OPERATORS '''
+# membership operators are used to test if a sequence is presented in an object
+
+numbers = [1,2,3,4,5]
+
+# in
+if x in numbers:
+    print(x in numbers)
+
+# not in
+if x not in numbers:
+    print(x in numbers)
+
+
+''' IDENTITY OPERATORS '''
+# compare the objects, not if they are equal
+# but if they are actually the same object, with same memory location
+
+# is
+if x is y:
+    print(x is y)
+
+# is not
+if x is not y:
+    print(x is not y)
